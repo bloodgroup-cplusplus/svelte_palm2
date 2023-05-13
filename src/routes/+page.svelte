abbe2525e7d2e8150597258ed18d5e1fe29d7223
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-05-13 08:49:54 +0530

    added firebase config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwQbeL7bX2mXcV4OFfxJ_cQfcWg_xVfHM",
+  authDomain: "sveltepalm2.firebaseapp.com",
+  projectId: "sveltepalm2",
+  storageBucket: "sveltepalm2.appspot.com",
+  messagingSenderId: "299530971549",
+  appId: "1:299530971549:web:3a50c87b568c8283426774"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+</script>
+
+
+
+
+<h1>🏝 🦄  Palm Reader</h1>
